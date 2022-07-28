77b8d494d83cee8996af1fc676dabba9fd80b25e
Patrick-FRE <patrick.lin@antra.com>
2022-07-28 14:57:15 -0400

    update Promise

--- JS/asyncPrograming.js
+++ JS/asyncPrograming.js
@@ -129,37 +129,113 @@ function getCompanyDataWithPromise(id) {
 //     })
 
 
+class MyPromise {
+    constructor(excutionFn) {
+        this.state = "PENDING"
+
+        const resolve = (value) => {
+            this.value = value;
+            this.state = "FULFIlLL";
+            console.log('resolve is working')
+            if (this.onFulfillmentFn) {
+                this.onFulfillmentFn(this.value)
+            }
+
+        }
+        const reject = (value) => {
+            this.state = "REJECT"
+            console.log('reject is working')
+            this.value = value;
+            if (this.onRejected) {
+                this.onRejected(this.value)
+            }
+
+        }
+
+        excutionFn(resolve, reject)
+    }
+    then(onFulfillmentFn, onRejected) {
+        let state = this.state;
+        let value = this.value;
+        return new MyPromise((res, rej) => {
+            setTimeout(() => {
+                if (state === "FULFIlLL") {
+                    let result = onFulfillmentFn(value);
+                    res(result)
+                } else if (state === "REJECT") {
+                    let result = onRejected(value);
+                    rej(result)
+                }
 
 
-// const myPromise = new Promise((resolve, reject) => {
-//     try {
-//         console.log("promise is running")
-//         resolve("hello")
-//         // throw new Error(
-//         //     "error"
-//         // )
-//     }
-//     catch (err) {
-//         reject("err")
-//     }
+                else {
+                    if (onFulfillmentFn) {
+                        this.onFulfillmentFn = (value) => {
+                            let result = onFulfillmentFn(value);
+                            res(result)
+                        }
+                    }
+                    if (onRejected) {
+                        this.onRejected = (value) => {
+                            let result = onRejected(value);
+                            rej(result)
+                        }
+                    }
 
-// });
+                }
 
-// myPromise
-//     .then((data) => {
-//         console.log("then is running", data)
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     })
+            })
+        })
+    }
+    catch() {
 
-
-function foo() {
-    for (var i = 0; i < 3; i++) {
-        setTimeout(() => {
-            console.log(i)
-        }, i * 1000)
     }
 }
 
-foo()
+
+
+const myPromise = new MyPromise((resolve, reject) => {
+    try {
+        console.log("promise is running")
+        setTimeout(() => {
+            try {
+                throw new Error('ERROR')
+                resolve({ msg: "resolve data after 2s" })
+            } catch (err) {
+                reject("err")
+            }
+        }, 2000)
+    }
+    catch (err) {
+        reject("err")
+    }
+});
+
+
+myPromise
+    .then((data) => {
+        console.log("then is running", data)
+        return 5
+    }, (err) => {
+        console.warn(err)
+    }).then((data2) => {
+        console.log("then2 is running", data2)
+    })
+
+console.log("hello")
+    // .catch((err) => {
+    //     console.log(err)
+    // })
+
+
+// function foo() {
+//     // 1. var will hoist to the top of the function scope
+//     for (var i = 0; i < 3; i++) {
+//         (function (input) {
+//             setTimeout(() => {
+//                 console.log(input)
+//             }, input * 1000)
+//         })(i)
+//     }
+// }
+// foo()
